40a7a958ca17bc6bbd99a01ad6a98ba1cd3f1365
edi <polodeanu.eduard@gmail.com>
2025-03-17 15:30:19 +0200

    Complete task

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # top_etch-a-sketch
+
+The Odin Project: Etch-a-sketch project
+
+This is the Etch-a-sketch project from The Odin Project course. The assignment is to make a web page where the user can interact with a part of the page and "draw" on it by changing the background color of the hovered elements, similar to a drawing board or a etch-a-sketch board. The user can change the size of the board.
+
+The project showcases basic methods for DOM manipulation and events. 
---
+++ script.js
@@ -0,0 +1,68 @@
+
+const INITIAL_SIZE = 16;
+
+const container = document.querySelector('#container');
+
+function createCards(size){
+    for (let i = 0; i < size**2; i++) {
+        let div = document.createElement('div');
+        div.classList.add('card');
+        container.appendChild(div);
+    }
+    const cards = document.querySelectorAll('.card');
+    setDimensions(cards, size);
+    console.log(`Created ${size ** 2} cards.`);
+}
+
+function setDimensions(cards, size){
+    const containerWidth = containerHeight = container.offsetWidth;
+    const cardWidth = cardHeight = containerWidth / size;
+    console.log("Container size: " + containerWidth + ' x ' + containerHeight);
+    console.log("Cards size: " + cardWidth + ' x ' + cardHeight);
+    cards.forEach((card) => {
+        card.setAttribute("style", "width:" + cardWidth + "px; height:" + cardHeight + "px;");
+    });
+}
+
+createCards(INITIAL_SIZE);
+
+function checkHoverEvent() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        card.addEventListener('mouseover', () => {
+            card.style.backgroundColor = "black";
+        });
+    });
+}
+checkHoverEvent();
+
+const newGridBtn = document.querySelector('#newGridBtn');
+newGridBtn.addEventListener('click', () => {
+    const boardWidth = container.offsetWidth;
+    const boardHeight = container.offsetHeight;
+
+    let newBoardSize = checkPromptText();
+
+    removeChildren(container);
+
+    createCards(newBoardSize);
+    checkHoverEvent();
+});
+
+function removeChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.lastChild);
+    }
+}
+
+function checkPromptText() {
+    const promptValue = prompt ("Enter the new board size: (max 100)");
+    const value = parseInt(promptValue);
+    if (isNaN(value))
+        alert("Input value not valid.");
+    else if (value > 100) {
+        alert("Input value too large. Value was set to 100.");
+        return 100;
+    }
+    else return value;
+}
